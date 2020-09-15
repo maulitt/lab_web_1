1e1836be42feb2c335ed9342491761eee0301c8c
regina <brf001@campus.mephi.ru>
2020-09-15 15:59:14 +0300

    1 task

---
+++ task_1.js
@@ -0,0 +1,29 @@
+function FirstOne(stroka){
+    //найти первый не повторяющийся символ в строке
+    let array_for_each_symbol = [];
+    for (let i = 0; i < stroka.length; i++)
+    {
+        array_for_each_symbol.push(0);
+    }
+    for (let j = 0; j < stroka.length; j++)
+    {
+        for (let i = 0; i < stroka.length; i++)
+        {
+            if (stroka[j] == stroka[i])
+            {
+                array_for_each_symbol[j]++;
+            }
+        }
+    }
+    let i = 0;
+    while(i < stroka.length)
+    {
+        if (array_for_each_symbol[i] == 1)
+        {
+            return stroka[i];
+            break;
+        }
+        i++;
+    }
+    return 0;
+}
