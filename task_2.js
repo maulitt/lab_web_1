ce3cc0e2d10bb5211624d057b9eceffc142bf2a4
regina <brf001@campus.mephi.ru>
2020-09-16 20:29:33 +0300

    1 task

--- task_2.js
+++ task_2.js
@@ -18,10 +18,10 @@ function SecondOne(stroka)
             array_of_numbers.push(i);
         }
     }
-    let result = '';
+    let result = stroka[0].toUpperCase();
     let mark = false;
     let another_mark = false;
-    for(let i = 0; i < stroka.length; i++)
+    for(let i = 1; i < stroka.length; i++)
     {
         mark = false;
         for(let j = 0; j < array_of_numbers.length; j++)
@@ -45,5 +45,7 @@ function SecondOne(stroka)
             another_mark = true;
         }
     }
-    return result;
+    console.log(result);
 }
+
+SecondOne('hi my name is maulitt');
